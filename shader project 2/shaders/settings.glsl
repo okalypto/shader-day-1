280d2b7a62bd803801bc79527c449ca1b4258348
Okalypto <orsokalypto@gmail.com>
2026-03-19 00:16:19 +0100

    day 2 shader

---
+++ shader project 2/shaders/settings.glsl
@@ -0,0 +1,8 @@
+#define BLUE_AMOUNT 0.0 //[0.0 0.25 0.5 0.75 1.0]
+#define GREEN_AMOUNT 0.0 //[0.0 0.25 0.5 0.75 1.0]
+#define GRAY_AMOUNT 0.0 //[0.0 0.25 0.5 0.75 1.0]
+#define BLACK_SKY 0 //[0 1]
+#define RED_SUN 0 //[0 1]
+#define GREEN_CLOUDS 0 //[0 1]
+#define RED_ENTITIES 0 //[0 1]
+#define TERRAIN_GRAY_AMOUNT 0.0 //[0.0 0.25 0.5 0.75 1.0]
